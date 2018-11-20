d3700c29bfd16e0f6d9ca671b44efb635786d627
Mirai Hattori <m-hattori@jsk.t.u-tokyo.ac.jp>
2018-11-20 12:53:28 +0900

    remove test; fix typo

--- rtc/SequencePlayer/SequencePlayer.cpp
+++ rtc/SequencePlayer/SequencePlayer.cpp
@@ -559,7 +559,7 @@ bool SequencePlayer::setJointAnglesSequenceFull(const OpenHRP::dSequenceSequence
     return m_seq->setJointAnglesSequenceFull(v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals, v_tms);
 }
 
-bool SequencePlayer::setJointAnglesSequenceFullWithBSpline(short i_bspline_recursive_order, short i_bspline_id, double i_bspline_tmin, double i_bspline_tmax, const OpenHRP::dSequence i_bspline_p, const OpenHRP::dSequenceSequence i_jvss, const OpenHRP::dSequenceSequence i_vels, const OpenHRP::dSequenceSequence i_torques, const OpenHRP::dSequenceSequence i_poss, const OpenHRP::dSequenceSequence i_rpys, const OpenHRP::dSequenceSequence i_accs, const OpenHRP::dSequenceSequence i_zmps, const OpenHRP::dSequenceSequence i_wrenches, const OpenHRP::dSequenceSequence i_optionals, const dSequence i_tms)
+bool SequencePlayer::setJointAnglesSequenceFullWithBSpline(short i_bsorder, short i_bsid, double i_bstmin, double i_bstmax, const OpenHRP::dSequence i_bsp, const OpenHRP::dSequenceSequence i_jvss, const OpenHRP::dSequenceSequence i_vels, const OpenHRP::dSequenceSequence i_torques, const OpenHRP::dSequenceSequence i_poss, const OpenHRP::dSequenceSequence i_rpys, const OpenHRP::dSequenceSequence i_accs, const OpenHRP::dSequenceSequence i_zmps, const OpenHRP::dSequenceSequence i_wrenches, const OpenHRP::dSequenceSequence i_optionals, const dSequence i_tms)
 {
     if ( m_debugLevel > 0 ) {
         std::cerr << __PRETTY_FUNCTION__ << std::endl;
@@ -570,8 +570,8 @@ bool SequencePlayer::setJointAnglesSequenceFullWithBSpline(short i_bspline_recur
 
     int len = i_jvss.length();
     std::vector<const double*> v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals;
-    std::vector<double> v_bspline_p, v_tms;
-    for ( unsigned int i = 0; i < i_bspline_p.length(); i++ ) v_bspline_p.push_back(i_bspline_p[i]);
+    std::vector<double> v_bsp, v_tms;
+    for ( unsigned int i = 0; i < i_bsp.length(); i++ ) v_bsp.push_back(i_bsp[i]);
     for ( unsigned int i = 0; i < i_jvss.length(); i++ ) v_jvss.push_back(i_jvss[i].get_buffer());
     for ( unsigned int i = 0; i < i_vels.length(); i++ ) v_vels.push_back(i_vels[i].get_buffer());
     for ( unsigned int i = 0; i < i_torques.length(); i++ ) v_torques.push_back(i_torques[i].get_buffer());
@@ -582,35 +582,8 @@ bool SequencePlayer::setJointAnglesSequenceFullWithBSpline(short i_bspline_recur
     for ( unsigned int i = 0; i < i_wrenches.length(); i++ ) v_wrenches.push_back(i_wrenches[i].get_buffer());
     for ( unsigned int i = 0; i < i_optionals.length(); i++ ) v_optionals.push_back(i_optionals[i].get_buffer());
     for ( unsigned int i = 0; i < i_tms.length();  i++ )  v_tms.push_back(i_tms[i]);
-    return m_seq->setJointAnglesSequenceFullWithBSpline(i_bspline_recursive_order, i_bspline_id, i_bspline_tmin, i_bspline_tmax, v_bspline_p, v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals, v_tms);
+    return m_seq->setJointAnglesSequenceFullWithBSpline(i_bsorder, i_bsid, i_bstmin, i_bstmax, v_bsp, v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals, v_tms);
 }
-
-/*
-bool SequencePlayer::setJointAnglesSequenceFullTest(const OpenHRP::dSequenceSequence i_jvss, const OpenHRP::dSequenceSequence i_vels, const OpenHRP::dSequenceSequence i_torques, const OpenHRP::dSequenceSequence i_poss, const OpenHRP::dSequenceSequence i_rpys, const OpenHRP::dSequenceSequence i_accs, const OpenHRP::dSequenceSequence i_zmps, const OpenHRP::dSequenceSequence i_wrenches, const OpenHRP::dSequenceSequence i_optionals, const dSequence i_tms)
-{
-    if ( m_debugLevel > 0 ) {
-        std::cerr << __PRETTY_FUNCTION__ << std::endl;
-    }
-    Guard guard(m_mutex);
-
-    if (!setInitialState()) return false;
-
-    int len = i_jvss.length();
-    std::vector<const double*> v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals;
-    std::vector<double> v_tms;
-    for ( unsigned int i = 0; i < i_jvss.length(); i++ ) v_jvss.push_back(i_jvss[i].get_buffer());
-    for ( unsigned int i = 0; i < i_vels.length(); i++ ) v_vels.push_back(i_vels[i].get_buffer());
-    for ( unsigned int i = 0; i < i_torques.length(); i++ ) v_torques.push_back(i_torques[i].get_buffer());
-    for ( unsigned int i = 0; i < i_poss.length(); i++ ) v_poss.push_back(i_poss[i].get_buffer());
-    for ( unsigned int i = 0; i < i_rpys.length(); i++ ) v_rpys.push_back(i_rpys[i].get_buffer());
-    for ( unsigned int i = 0; i < i_accs.length(); i++ ) v_accs.push_back(i_accs[i].get_buffer());
-    for ( unsigned int i = 0; i < i_zmps.length(); i++ ) v_zmps.push_back(i_zmps[i].get_buffer());
-    for ( unsigned int i = 0; i < i_wrenches.length(); i++ ) v_wrenches.push_back(i_wrenches[i].get_buffer());
-    for ( unsigned int i = 0; i < i_optionals.length(); i++ ) v_optionals.push_back(i_optionals[i].get_buffer());
-    for ( unsigned int i = 0; i < i_tms.length();  i++ )  v_tms.push_back(i_tms[i]);
-    return m_seq->setJointAnglesSequenceFullTest(v_jvss, v_vels, v_torques, v_poss, v_rpys, v_accs, v_zmps, v_wrenches, v_optionals, v_tms);
-}
-*/
 
 bool SequencePlayer::setBasePos(const double *pos, double tm)
 {
